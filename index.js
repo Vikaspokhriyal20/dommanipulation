ce9e072200e99c3f74b1d63ca7af1917a7e7474a
vikas singh <pokhriyalvikas940@gmail.com>
2023-09-29 09:28:23 +0530

    new

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// Dom Targeting Method
+let menu = document.getElementById('menu');
+let list = document.createElement('li');
+list.innerText = 'Contact';
+menu.appendChild(list);
+
+let inputbox = document.getElementById('inputbox');
+function setAttributes(params) {
+    inputbox.setAttribute('disabled', '');
+}
+
+
 
